3ef3984217ffeaf029ba5be6e20390e7f22cbfc7
HeesuJu <hsjoo2442@gmail.com>
2023-10-29 16:26:23 +0900

    added utils

---
+++ utils.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+
+h_pad = 10
+v_pad = 5
+
+def style_label_as_entry(label, color):
+    label.config(relief=tk.SUNKEN, borderwidth=1)  # Add a border
+    label.config(bg=color)  # Set background color to white
+    label.config(font=("Helvetica", 9))  # Set font style and size
+
+class WrappingLabel(tk.Label):
+    def __init__(self, master=None, **kwargs):
+        tk.Label.__init__(self, master, **kwargs)
+        self.bind('<Configure>', lambda e: self.config(wraplength=self.winfo_width()))
