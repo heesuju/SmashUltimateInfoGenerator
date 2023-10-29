3e2801338648b1d08f286791351c706e6216a368
HeesuJu <hsjoo2442@gmail.com>
2023-10-29 16:19:20 +0900

    fixed problem with missing elements in info.toml

--- loader.py
+++ loader.py
@@ -2,21 +2,26 @@ import tomli as tomli
 
 class Loader:
     def __init__(self):
+        self.reset()
+    
+    def reset(self):
         self.display_name = ""
         self.description = ""
         self.authors = ""
         self.category = ""
         self.version = ""
-    
+
     def load_toml(self, dir):
+        self.reset()
+
         with open(dir + "\info.toml", "rb") as toml_file:
             loaded_dict = tomli.load(toml_file)
             if loaded_dict:
-                self.display_name = loaded_dict["display_name"]
-                self.description = loaded_dict["description"]
-                self.authors = loaded_dict["authors"]
-                self.category = loaded_dict["category"]
-                self.version = loaded_dict["version"]
+                if "display_name" in loaded_dict: self.display_name = loaded_dict["display_name"]
+                if "description" in loaded_dict: self.description = loaded_dict["description"]
+                if "authors" in loaded_dict: self.authors = loaded_dict["authors"]
+                if "category" in loaded_dict: self.category = loaded_dict["category"]
+                if "version" in loaded_dict: self.version = loaded_dict["version"]
             
                 print("successfully loaded info.toml in working directory")
                 return True
