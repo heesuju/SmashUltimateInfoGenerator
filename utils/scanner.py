4c327e8ab47eb0702840aac23f58113093a6b5a4
HeesuJu <hsjoo2442@gmail.com>
2024-04-21 21:00:20 +0900

    added parallel processing

--- utils/scanner.py
+++ utils/scanner.py
@@ -1,5 +1,6 @@
 import os
 from threading import Thread
+import concurrent.futures
 from utils.loader import Loader
 from utils.generator import Generator
 from mod import Mod
@@ -10,8 +11,6 @@ class Scanner(Thread):
     def __init__(self, directory:str, callback):
         super().__init__()
         self.directory = directory
-        self.loader = Loader()
-        self.generator = Generator()
         self.callback = callback
         
     def find_image(self, directory, width, height):
@@ -28,40 +27,49 @@ class Scanner(Thread):
 
         return ImageTk.PhotoImage(img.resize((new_width, new_height), Image.Resampling.LANCZOS))
     
+    def find_mod(self, name:str, path:str):
+        if os.path.isdir(path):
+            mod = Mod(name, name, "", "", "", "", "", "", False)
+            mod.path = path
+            loader = Loader()
+            generator = Generator()
+            if loader.load_toml(path):
+                mod.display_name = loader.display_name
+                mod.authors = loader.authors
+                mod.category = loader.category
+                mod.version = loader.version
+                mod.info_toml = True
+            
+            generator.preview_info_toml(path, "", "", "")
+            mod.slots = common.slots_to_string(generator.slots)
+            mod.characters = common.group_char_name(generator.char_names, generator.group_names)      
+            
+            mod.mod_name = mod.display_name.replace(mod.slots.lower(), "")
+            mod.mod_name = mod.mod_name.replace(mod.slots.lower().replace(" ", ""), "")
+            mod.mod_name = mod.mod_name.replace(mod.slots.upper(), "")
+            mod.mod_name = mod.mod_name.replace(mod.slots.upper().replace(" ", ""), "")
+            
+            mod.mod_name = mod.mod_name.replace(mod.characters, "")
+            mod.mod_name = mod.mod_name.replace(mod.category, "")
+            mod.mod_name = common.trim_redundant_spaces(mod.mod_name)
+            img_path = path + "/preview.webp"
+            if os.path.exists(img_path):
+                mod.img = self.find_image(path + "\\preview.webp", 100, 60)
+
+            return mod
+        return None
+    
     def find_mods(self, directory):
         mods = []
 
         if os.path.isdir(directory):
-            for folder_name in os.listdir(directory):
-                
-                folder_path = os.path.join(directory, folder_name)
-                if os.path.isdir(folder_path):
-                    mod = Mod(folder_name, folder_name, "", "", "", "", "", "", False)
-                    mods.append(mod)
-                    mod.path = folder_path
-
-                    if self.loader.load_toml(folder_path):
-                        mod.display_name = self.loader.display_name
-                        mod.authors = self.loader.authors
-                        mod.category = self.loader.category
-                        mod.version = self.loader.version
-                        mod.info_toml = True
-                    
-                    self.generator.preview_info_toml(folder_path, "", "", "")
-                    mod.slots = common.slots_to_string(self.generator.slots)
-                    mod.characters = common.group_char_name(self.generator.char_names, self.generator.group_names)      
-                    
-                    mod.mod_name = mod.display_name.replace(mod.slots.lower(), "")
-                    mod.mod_name = mod.mod_name.replace(mod.slots.lower().replace(" ", ""), "")
-                    mod.mod_name = mod.mod_name.replace(mod.slots.upper(), "")
-                    mod.mod_name = mod.mod_name.replace(mod.slots.upper().replace(" ", ""), "")
-                    
-                    mod.mod_name = mod.mod_name.replace(mod.characters, "")
-                    mod.mod_name = mod.mod_name.replace(mod.category, "")
-                    mod.mod_name = common.trim_redundant_spaces(mod.mod_name)
-                    img_path = folder_path + "/preview.webp"
-                    if os.path.exists(img_path):
-                        mod.img = self.find_image(folder_path + "\\preview.webp", 100, 60)
+            
+            with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+                futures = [executor.submit(self.find_mod, folder_name, os.path.join(directory, folder_name)) for folder_name in os.listdir(directory)]
+                for future in concurrent.futures.as_completed(futures):
+                    mod = future.result()
+                    if mod is not None:
+                        mods.append(mod)
 
         self.callback(mods)
 
