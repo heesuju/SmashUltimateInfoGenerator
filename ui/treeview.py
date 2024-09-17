f84fd719e114242e3807ae648e02e6e6e991d609
HeesuJu <hsjoo2442@gmail.com>
2024-09-17 22:58:51 +0900

    open config window when no mods are detected

--- ui/treeview.py
+++ ui/treeview.py
@@ -58,17 +58,19 @@ class Menu:
 
     def populate(self, mods):
         self.total_pages = math.ceil(len(mods)/self.page_size)
-        print(f"found {len(mods)} items")
-        
-        self.show_paging()
-        start = (self.cur_page-1) * self.page_size
-        end = common.clamp(self.cur_page * self.page_size, start, len(mods))
-        for n in range(start,end):
-            if mods[n]["img"] == None: self.treeview.insert("", tk.END, values=(mods[n]["mod_name"], mods[n]["category"], mods[n]["authors"], mods[n]["characters"], mods[n]["slots"], mods[n]["path"]))
-            else: self.treeview.insert("", tk.END, image=mods[n]["img"], values=(mods[n]["mod_name"], mods[n]["category"], mods[n]["authors"], mods[n]["characters"], mods[n]["slots"], mods[n]["path"]))
-        
-        n_count = end - start
-        self.l_page.config(text=f"{n_count} of {len(mods)}")
+        if len(mods) > 0:
+            print(f"found {len(mods)} items")
+            self.show_paging()
+            start = (self.cur_page-1) * self.page_size
+            end = common.clamp(self.cur_page * self.page_size, start, len(mods))
+            for n in range(start,end):
+                if mods[n]["img"] == None: self.treeview.insert("", tk.END, values=(mods[n]["mod_name"], mods[n]["category"], mods[n]["authors"], mods[n]["characters"], mods[n]["slots"], mods[n]["path"]))
+                else: self.treeview.insert("", tk.END, image=mods[n]["img"], values=(mods[n]["mod_name"], mods[n]["category"], mods[n]["authors"], mods[n]["characters"], mods[n]["slots"], mods[n]["path"]))
+            
+            n_count = end - start
+            self.l_page.config(text=f"{n_count} of {len(mods)}")
+        else:
+            self.open_config()
 
     def on_filter_submitted(self, event):
         self.search()
