8c32b87770835482e4f13c82476e4d35f03cd132
HeesuJu <hsjoo2442@gmail.com>
2024-09-26 08:51:19 +0900

    normalized edit distance

--- common.py
+++ common.py
@@ -270,15 +270,47 @@ def get_pages(current_page=1, total_pages=1, max_size=5):
     out_arr.sort()
     return out_arr
 
+def get_cleaned(text:str)->str:
+    text = text.lower()
+    text = text.replace(" ", "")
+    text = text.replace(".", "")
+    return text
+
 def get_completion(text:str, values:list)->None:
     if not text:
         return ""
     elif text not in values:
+        min_num = 20.0
+        min_text = ""
         for cat in values:
-            if text.lower() in cat.lower():
-                return cat
-        return ""
+            dist = get_edit_distance(get_cleaned(text), get_cleaned(cat))
+            if dist == min_num:
+                if get_cleaned(text) not in get_cleaned(min_text):
+                    min_text = cat
+            elif dist < min_num:
+                min_num = dist
+                min_text = cat
+        return min_text
     else:
         return text
     
 def get_edit_distance(text1:str, text2:str)->float:
+    if len(text1) > len(text2):
+        text1, text2 = text2, text1
+    
+    distances = range(len(text1) + 1)
+    for idx2, char2 in enumerate(text2):
+        distances_ = [idx2 + 1]
+        for idx1, char1 in enumerate(text1):
+            if char1 == char2:
+                distances_.append(distances[idx1])
+            else:
+                distances_.append(1 + min((distances[idx1], distances[idx1 + 1], distances_[-1])))
+        distances = distances_
+
+    def normalize(a:str, b:str, distance:float)->float:
+        max_len = max(len(a), len(b))
+        return distance/max_len
+
+    return normalize(text1, text2, distances[-1])
+
