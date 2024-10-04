ad2021605ff0a03e5c8faeb2912beff1702829b7
HeesuJu <hsjoo2442@gmail.com>
2024-10-04 10:42:16 +0900

    fixed directory name trimming bug

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ build_exe_options = {
 # change base to console to show console logs
 setup(
     name="Info Toml Generator",
-    version="1.6.0",
+    version="1.6.1",
     description="Info.toml Generator for Smash Ultimate",
     options={"build_exe": build_exe_options},
     executables=[Executable("main.py", base="Win32GUI")],
--- utils/scanner.py
+++ utils/scanner.py
@@ -7,7 +7,7 @@ from PIL import Image, ImageTk
 import common
 from .cleaner import extract_mod_name
 from .format import format_slots
-from .files import is_valid_dir
+from .files import is_valid_dir, get_dir_name
 from utils.hash import gen_hash_as_decimal
 from typing import Union
 
@@ -109,7 +109,7 @@ class Scanner(Thread):
             if isinstance(directory, str):
                 futures = [executor.submit(self.find_mod, folder_name, os.path.join(directory, folder_name)) for folder_name in os.listdir(directory)]
             elif isinstance(directory, list):
-                futures = [executor.submit(self.find_mod, d.split("\\")[-1], d) for d in directory]
+                futures = [executor.submit(self.find_mod, get_dir_name(d), d) for d in directory]
             if self.progress_callback is not None:
                 [future.add_done_callback(self.progress_callback) for future in futures]
             for future in concurrent.futures.as_completed(futures):
