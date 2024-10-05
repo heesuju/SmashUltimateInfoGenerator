1b6f8dd056c0c6d1ae71cbcb275c340845f3db8f
HeesuJu <hsjoo2442@gmail.com>
2024-10-05 22:54:23 +0900

    fixed toml read error

--- utils/loader.py
+++ utils/loader.py
@@ -21,21 +21,25 @@ class Loader:
         self.reset()
         if not os.path.exists(dir + "/info.toml"):
             return False
-        with open(dir + "/info.toml", "rb") as toml_file:
-            loaded_dict = tomli.load(toml_file)
-            if loaded_dict:
-                if "display_name" in loaded_dict: self.display_name = loaded_dict["display_name"]
-                if "description" in loaded_dict: self.description = loaded_dict["description"]
-                if "authors" in loaded_dict: self.authors = loaded_dict["authors"]
-                if "category" in loaded_dict: self.category = loaded_dict["category"]
-                if "version" in loaded_dict: self.version = loaded_dict["version"]
-                if "mod_name" in loaded_dict: self.mod_name = loaded_dict["mod_name"]
-                if "url" in loaded_dict: self.url = loaded_dict["url"]
-                if "wifi_safe" in loaded_dict: self.wifi_safe = loaded_dict["wifi_safe"]
-                if "includes" in loaded_dict: self.includes = loaded_dict["includes"]
-                if "slots" in loaded_dict: self.slots = loaded_dict["slots"]
-            
-                print("successfully loaded info.toml in working directory")
-                return True
-            else:
-                return False
+        try:
+            with open(dir + "/info.toml", "rb") as toml_file:
+                loaded_dict = tomli.load(toml_file)
+                if loaded_dict:
+                    if "display_name" in loaded_dict: self.display_name = loaded_dict["display_name"]
+                    if "description" in loaded_dict: self.description = loaded_dict["description"]
+                    if "authors" in loaded_dict: self.authors = loaded_dict["authors"]
+                    if "category" in loaded_dict: self.category = loaded_dict["category"]
+                    if "version" in loaded_dict: self.version = loaded_dict["version"]
+                    if "mod_name" in loaded_dict: self.mod_name = loaded_dict["mod_name"]
+                    if "url" in loaded_dict: self.url = loaded_dict["url"]
+                    if "wifi_safe" in loaded_dict: self.wifi_safe = loaded_dict["wifi_safe"]
+                    if "includes" in loaded_dict: self.includes = loaded_dict["includes"]
+                    if "slots" in loaded_dict: self.slots = loaded_dict["slots"]
+                
+                    print("successfully loaded info.toml in working directory")
+                    return True
+                else:
+                    return False
+        except Exception as e:
+            print("Error while reading toml", e)
+            return False
