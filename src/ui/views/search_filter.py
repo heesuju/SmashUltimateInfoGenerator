45c78046311c41a0a24f9965723cca32f8e7b247
HeesuJu <hsjoo2442@gmail.com>
2025-01-12 12:35:18 +0900

    fixed focus order on filter, removed redundant focus

--- src/ui/views/search_filter.py
+++ src/ui/views/search_filter.py
@@ -97,29 +97,29 @@ class SearchFilter:
         self.frame_slots.grid(row=0, column=5, sticky=tk.EW, pady=PAD_V/2)
         vcmd = (self.root.register(validate_slot)) 
 
-        self.cbox_slots = ttk.Combobox(self.frame_slots, values=SLOT_RULE, width=8, textvariable=self.slot_rule_var)
+        self.cbox_slots = ttk.Combobox(self.frame_slots, values=SLOT_RULE, width=6, textvariable=self.slot_rule_var)
         self.cbox_slots.pack(side=tk.LEFT, fill=tk.X, expand=False, padx=(0,PAD_H))
         self.cbox_slots.bind("<Return>", self.on_submit)
         self.slot_rule_var.set(SLOT_RULE[0])
 
-        self.entry_slots_from = tk.Entry(self.frame_slots, width=5, validate='all', validatecommand=(vcmd, '%P'), textvariable=self.slot_from_var)
+        self.entry_slots_from = tk.Entry(self.frame_slots, width=3, validate='all', validatecommand=(vcmd, '%P'), textvariable=self.slot_from_var)
         self.entry_slots_from.pack(side=tk.LEFT, fill=tk.X, expand=True)
         self.entry_slots_from.bind("<Return>", self.on_submit)
         add_hint_text(self.entry_slots_from, "0")
 
-        label_slot_n = ttk.Label(self.frame_slots, text="~")
-        label_slot_n.pack(side=tk.LEFT, padx=PAD_H)
+        label_slot_n = ttk.Label(self.frame_slots, text="~", takefocus=False)
+        label_slot_n.pack(side=tk.LEFT, padx=PAD_H/2)
         label_slot_n.bind("<Return>", self.on_submit)
 
-        self.entry_slots_to = tk.Entry(self.frame_slots, width=5, validate='all', validatecommand=(vcmd, '%P'), textvariable=self.slot_to_var)
+        self.entry_slots_to = tk.Entry(self.frame_slots, width=3, validate='all', validatecommand=(vcmd, '%P'), textvariable=self.slot_to_var)
         self.entry_slots_to.pack(side=tk.LEFT, fill=tk.X, expand=True)
         self.entry_slots_to.bind("<Return>", self.on_submit)
         add_hint_text(self.entry_slots_to, "255")
 
-        self.cbox_category = add_filter_dropdown(self.frame, 1, 4, "Category", [DEFAULT_VALUE] + CATEGORIES, self.category_var, self.on_submit, False)
         self.cbox_series = add_filter_dropdown(self.frame, 1, 0, "Series", self.params.series_list, self.series_var, self.on_submit)
         self.cbox_series.bind("<<ComboboxSelected>>", self.on_series_changed)
         self.cbox_char = add_filter_dropdown(self.frame, 1, 2, "Character", self.params.characters_list, self.character_var, self.on_submit)
+        self.cbox_category = add_filter_dropdown(self.frame, 1, 4, "Category", [DEFAULT_VALUE] + CATEGORIES, self.category_var, self.on_submit, False)
 
         self.cbox_info = add_filter_dropdown(self.frame, 2, 0, "Info.toml", self.params.info_toml_list, self.info_toml_var, self.on_submit)
         self.cbox_wifi = add_filter_dropdown(self.frame, 2, 2, "Wifi-Safe", self.params.wifi_safe_list, self.wifi_safe_var, self.on_submit)
