1bbdb5263aa19711fd6c34a84b27c89194e4e43c
HeesuJu <hsjoo2442@gmail.com>
2025-01-14 23:29:22 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ build_exe_options = {
     "packages": ["tkinter", "tkinterdnd2", "webdriver_manager", "selenium"],
     "includes": ["tkinterdnd2", "webdriver_manager", "selenium"],    
     "include_files": ["assets", "data", "src"],
-    "zip_include_packages": ["tomli_w", "Pillow", "requests", "beautifulsoup4", "tomli"],
+    "zip_include_packages": ["tomli_w", "Pillow", "requests", "beautifulsoup4", "tomli", "pyunpack", "patool"],
 }
 
 # change base to console to show console logs
 setup(
     name="Info Toml Generator",
-    version="1.8.0",
+    version="1.8.2",
     description="Info.toml Generator for ARCropolis",
     options={"build_exe": build_exe_options},
     executables=[Executable("main.py", base="Win32GUI")],
